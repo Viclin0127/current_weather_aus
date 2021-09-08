67dff183139a9c1a8c975b5f41bfc6656fe0ebe0
YuanHungLin <viclin@linyuanhongde-MacBook-Pro.local>
2021-09-08 21:20:04 +1000

    add responsive-layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,15 +9,17 @@ import Footer from './Footer';
 
 function Main() {
     const [data, setData] = useState({})
+    const [display, setDisplay] = useState(false)
 
     const onChangeSelected = async e =>{
         try{
             const {data} = await openweather.get("/weather", {params: {q: e.value, units: "metric"}})
             setData({wind: data.wind.speed, temp: data.main.temp, hum: data.main.humidity, weather: data.weather[0].main})
-            console.log(data)
+            setDisplay(true)
         }
         catch(ex){
-            console.log(ex.message)
+            // TODO: Error handling
+            alert("Error from connecting to the API...")
         }
     }
 
@@ -30,18 +32,23 @@ function Main() {
                     Current Weather Search
                 </div>
                 <Dropdown className="my-dropdown" options={options} onChange={onChangeSelected} placeholder="Select a City"/>
-                <div className="display">
-                    <div className="display-content">
-                        <React.Fragment>
-                            <ul className="content-wrapper">
-                                <li className="content-item">{`${data.temp} ºC`}</li>
-                                <li className="content-item">{data.wind}<BiWind className="content-icon"/></li>
-                                <li className="content-item">{data.hum}<WiHumidity className="content-icon"/></li>
-                                <li className="content-item">{`(${data.weather})`}</li>
-                            </ul>
-                        </React.Fragment>
+                { display ? 
+                    <div className="display">
+                        <div className="display-content">
+                            <React.Fragment>
+                                <ul className="content-wrapper">
+                                    <li className="content-item">{`${data.temp} ºC`}</li>
+                                    <li className="content-item">{data.wind}<BiWind className="content-icon"/></li>
+                                    <li className="content-item">{data.hum}<WiHumidity className="content-icon"/></li>
+                                    <li className="content-item">{`(${data.weather})`}</li>
+                                </ul>
+                            </React.Fragment>
+                        </div>
                     </div>
-                </div>
+                : <div className="display">
+                    No City Selected...
+                    </div>
+                }
                 <Footer/>
             </div>
         </div> 
